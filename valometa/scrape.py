a27ea6a9abb303a60b5411fb24d02b0d59d4709e
Kobesani Pogo <harold.rockata@gmail.com>
2022-02-17 00:21:00 +0100

    Adding date extractor class for vlr.gg/matches/results pages

--- valometa/scrape.py
+++ valometa/scrape.py
@@ -1,4 +1,39 @@
+from typing import Iterator
+
 import parsel
+
+class DateExtractor(object):
+    """
+    A class extracting the dates from the vlr.gg/matches/results pages
+
+    Attributes
+    ----------
+    selector: parsel.Selector
+        a parsel Selector on which one can call xpath and getall methods
+    
+    Methods
+    -------
+    yield_data() -> Iterator[str]
+        returns dates in text format from a results page at vlr.gg 
+
+    """
+
+    def __init__(self, selector: parsel.Selector) -> None:
+        self.selector = selector
+
+    def yield_data(self) -> Iterator[str]:
+        data = (
+            self
+            .selector
+            .xpath("//div[@class='wf-label mod-large']")
+            .xpath("normalize-space(./text())")
+            .getall()
+        )
+
+        for datum in data:
+            yield datum
+
+
 
 class ResultsCardScraper:
     pass
