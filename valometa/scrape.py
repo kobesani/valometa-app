9051c4517946262eefcc5e8198812155daac5f3a
Kobesani Pogo <harold.rockata@gmail.com>
2022-02-17 01:32:11 +0100

    Adding basics of Match extraction to repo

--- valometa/scrape.py
+++ valometa/scrape.py
@@ -1,6 +1,17 @@
-from typing import Iterator
+from datetime import datetime
+from difflib import Match
+from typing import Iterator, List
 
 import parsel
+
+from pydantic.dataclasses import dataclass
+
+@dataclass
+class MatchBasic:
+    timestamp: datetime
+    url: str
+    match_id: int
+
 
 class DateExtractor(object):
     """
@@ -34,10 +45,61 @@ class DateExtractor(object):
             yield datum
 
 
+class CardExtractor(object):
+    def __init__(self, selector: parsel.Selector) -> None:
+        self.selector = selector
 
-class ResultsCardScraper:
-    pass
+    def yield_data(self) -> List[parsel.Selector]:
+        data = (
+            self
+            .selector
+            .xpath("//div[@class='wf-card']")
+        )
+
+        for datum in data:
+            yield datum
 
 
-class MatchScraper:
-    pass
+class MatchExtractor(object):
+    def __init__(self, selector: parsel.Selector) -> None:
+        self.selector = selector
+        self.date_extractor = DateExtractor(self.selector)
+        self.card_extractor = CardExtractor(self.selector)
+
+    def build_timestamp(self, date: str, match: parsel.Selector):
+        time = (
+            match
+            .xpath("./div[@class='match-item-time']")
+            .xpath("normalize-space(./text())")
+            .get()
+        )
+
+        utc_offset = (
+            datetime
+            .utcnow()
+            .astimezone()
+            .utcoffset()
+            .total_seconds()
+        )
+
+        utc_offset_hours = int(utc_offset / 3600)
+
+        return datetime.strptime(
+            f"{date}, {time} UTC+{utc_offset_hours:02}:00",
+            "%a, %B %d, %Y, %I:%M %p %Z%z"
+        )
+
+    def yield_data(self):
+        for date, card in zip(
+            self.date_extractor.yield_data(),
+            self.card_extractor.yield_data()
+        ):
+            for match in (
+                card.xpath("./a[contains(@class, 'wf-module-item match-item')]")
+            ):
+
+                yield MatchBasic(
+                    timestamp=self.build_timestamp(date, match),
+                    url=match.attrib['href'],
+                    match_id=int(match.attrib['href'].split("/")[1])
+                )
