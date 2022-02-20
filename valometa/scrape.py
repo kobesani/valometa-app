42f34ea88850dc9dc4b7fd6c2c5d3f7fdde383ab
Kobesani Pogo <harold.rockata@gmail.com>
2022-02-20 13:31:58 +0100

    Adding functionality to update, should work now... testing needed

--- valometa/scrape.py
+++ valometa/scrape.py
@@ -1,7 +1,8 @@
 import os
-from sqlite3 import IntegrityError
 import time
 
+from datetime import datetime
+from sqlite3 import IntegrityError
 from typing import Dict, Iterator, Optional
 
 import parsel
@@ -139,11 +140,25 @@ class MatchesUpdate(object):
 
         self.session = requests.Session()
         self.delay = delay
-        self.sqlite_db_path = sqlite_db_path    
+        self.sqlite_db_path = sqlite_db_path
+        self.latest_match: Optional[datetime] = None
+        self.update_finished: bool = False
 
     def set_up_updater(self) -> None:
         self.engine = create_engine(f"sqlite:///{self.sqlite_db_path}")
         self.db_session_maker = sessionmaker(bind=self.engine)
+        
+        # get most recent match
+        with self.db_session_maker() as sesh:
+            self.latest_match = (
+                sesh
+                .query(Matches.timestamp)
+                # sometimes timestamp isn't found and therefore is None, filter
+                .filter(Matches.timestamp.is_not(None))
+                .order_by(desc('timestamp'))
+                # datetime returned as tuple
+                .first()[0]
+            )
 
     def request(self) -> Optional[requests.models.Response]:
         with self.session as sesh:
@@ -169,21 +184,28 @@ class MatchesUpdate(object):
         matches_generator = MatchExtractor(main_select).yield_data()
 
         for match in matches_generator:
+            if match.timestamp < self.latest_match:
+                self.update_finished = True
+                break
             with self.db_session_maker() as sesh:
                 sesh.add(Matches(**match.asdict()))
                 try:
                     sesh.commit()
                 except IntegrityError as e:
+                    # non-unique primary key entry
                     # error is raised when games are shifted between pages
                     # due to games finishing and being added to page 1
                     print(e)
 
     def update_database(self) -> None:
-        self.set_up_builder()
+        self.set_up_updater()
         for x in range(1, self.max_pages + 1):
             print(f"Scraping Page {x}")
             self.current_page = x
             self.parse_response()
+            if self.update_finished:
+                print("Update finished")
+                return
             time.sleep(self.delay)
 
 # class ValorantResults:
