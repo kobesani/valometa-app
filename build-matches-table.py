2ea373964ca0febe10fc4647b47b6d117f7bbb51
Kobesani Pogo <harold.rockata@gmail.com>
2022-02-20 14:04:14 +0100

    Adding simple scripts for updating and building matches table in -> ./valometa/data/vlr-gg.db

---
+++ build-matches-table.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python3
+
+from valometa.scrape import MatchesBuild
+
+match_db_builder = MatchesBuild()
+match_db_builder.build_database()
---
+++ update-matches-table.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python3
+
+from valometa.scrape import MatchesUpdate
+
+match_db_updater = MatchesUpdate()
+match_db_updater.update_database()
