5140fd5b861f032dcace82e580001080aedad899
Kobesani Pogo <harold.rockata@gmail.com>
2022-02-28 20:04:52 +0100

    Added port argument and validation in run app script

--- run-app.sh
+++ run-app.sh
@@ -1,4 +1,26 @@
 #! /usr/bin/env bash
 
-poetry run uvicorn valometa.api.routes:app --reload --port 8000
+#!/bin/bash
 
+default_port=8000
+
+function to_int {
+    local -i num="10#${1}"
+    echo "${num}"
+}
+
+function port_is_ok {
+    local port="$1"
+    local -i port_num=$(to_int "${port}" 2>/dev/null)
+
+    if (( $port_num < 1 || $port_num > 65535 )) ; then
+        echo $default_port
+        return
+    fi
+
+    echo $port_num
+}
+
+port=$(port_is_ok $1)
+
+poetry run uvicorn valometa.api.routes:app --reload --port ${port}
