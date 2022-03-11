fc36daaadf0e77574696e2261fa941328b67422e
Kobe Rockata <harald.rockata@gmail.com>
2022-03-11 16:00:22 +0100

    Adding pydantic schemas, data retrieval functions for pandas and a basic route (route not finished)

--- valometa/api/schemas.py
+++ valometa/api/schemas.py
@@ -1,7 +1,15 @@
 from datetime import date
+from typing import List
 
-from pydantic import BaseModel
+from pydantic import BaseModel, confloat, validator, ValidationError, root_validator
 
+from valometa.data.raw import (
+    min_patch_version, max_patch_version
+)
+
+patch_constraint = confloat(
+    ge=min_patch_version, le=max_patch_version
+)
 
 class DateRange(BaseModel):
     date_begin: date
@@ -13,6 +21,29 @@ class NumberMatchesDay(BaseModel):
     count: int
 
 
+class PatchConstraintError(Exception):
+    pass
+
+
 class MapPatchFilter(BaseModel):
     map_name: str
-    patch: str
+    patch_lower: patch_constraint = min_patch_version
+    patch_upper: patch_constraint = max_patch_version
+
+    @root_validator
+    def lower_le_upper(cls, values):
+        lower, upper = (
+            values.get('patch_lower'), values.get('patch_upper')
+        )
+
+        # Nones if the confloat fails for lower or upper
+        if lower is not None and upper is not None:
+            if lower > upper:
+                raise PatchConstraintError(
+                    f"lower={lower} > upper={upper} not allowed."
+                )
+
+        return values
+
+
+
--- valometa/utils/data.py
+++ valometa/utils/data.py
@@ -1,11 +1,41 @@
+from typing import Optional
+
 import pandas
 
 
-def get_matches_per_day(match_cards_dataframe: pandas.DataFrame) -> pandas.DataFrame:
+def get_matches_per_day(
+    match_cards_dataframe: pandas.DataFrame
+) -> pandas.DataFrame:
     return (
         match_cards_dataframe
         .groupby(match_cards_dataframe.timestamp.dt.date)
         .size()
         .reset_index()
         .rename(columns={0: 'count'})
+    )
+
+
+def get_agent_pick_rates(
+    agents_dataframe: pandas.DataFrame,
+    map_name: str,
+    patch_lower: float,
+    patch_upper: float
+) -> pandas.DataFrame:
+    patch_filter = (
+        agents_dataframe
+        .query(f'patch <= {patch_upper}')
+        .query(f'patch >= {patch_lower}')
+    )
+
+    if map_name != "All":
+        map_filter = patch_filter.query(f'map_name == {map_name}')
+    
+    else:
+        map_filter = patch_filter
+
+    return (
+        map_filter
+        .groupby(['agent_name'])
+        .size()
+        .reset_index()
     )
