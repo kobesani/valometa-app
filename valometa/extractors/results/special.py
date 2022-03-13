bd83d517451dfdc5b3d821aecb255fc1e2dccb29
Kobesani Pogo <harold.rockata@gmail.com>
2022-03-13 21:19:21 +0100

    Fixing bug in pendulum timezone usage.. now always converts time to Europe/Vienna

--- valometa/extractors/results/special.py
+++ valometa/extractors/results/special.py
@@ -12,21 +12,7 @@ class Timestamp(object):
         self.date = date
         self.selector = selector
 
-        self.current_timezone = pendulum.now().tzinfo
-
-        # utc_offset_seconds = (
-        #     datetime
-        #     .utcnow()
-        #     .astimezone()
-        #     .utcoffset()
-        #     .total_seconds()
-        # )
-
-        # self.utc_offset = int(utc_offset_seconds / 3600)
-
-        # assert self.utc_offset == utc_offset_seconds / 3600, (
-        #     "UTC offset is not a whole hour number"
-        # )
+        self.current_timezone = pendulum.now().timezone
 
     def yield_data(self) -> Optional[datetime]:
         time_data = (
@@ -42,13 +28,8 @@ class Timestamp(object):
         
         ts = f"{self.date}, {time_data}"
 
-        return self.timezone.convert(
+        return DEFAULT_TIMEZONE.convert(
             pendulum.from_format(
-                ts, "ddd, MMMM DD, YYYY, hh:mm A", tz=DEFAULT_TIMEZONE
+                ts, "ddd, MMMM DD, YYYY, hh:mm A", tz=self.current_timezone
             )
         )
-
-        # return datetime.strptime(
-        #     f"{self.date}, {time} UTC+{self.utc_offset:02}:00",
-        #     "%a, %B %d, %Y, %I:%M %p %Z%z"
-        # )
